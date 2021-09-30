f9f417951becf48966f037ec7816a5f96e4bb0e1
aeither <36173828+aeither@users.noreply.github.com>
2021-09-30 13:49:50 +0200

    .

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,23 +3,19 @@ const defaultSEOConfig = {
   title: "web3-converter",
   titleTemplate: "%s | web3-converter",
   defaultTitle: "web3-converter",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://web3-converter.sznm.dev",
+  description: "Convert ether to wei and string to bytes32",
+  canonical: "https://web3-converter.vercel.app",
   openGraph: {
-    url: "https://web3-converter.sznm.dev",
+    url: "https://web3-converter.vercel.app",
     title: "web3-converter",
-    description: "Next.js + chakra-ui + TypeScript template",
+    description: "Convert ether to wei and string to bytes32",
     images: [
       {
         url: "https://og-image.sznm.dev/**web3-converter**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "web3-converter.sznm.dev og-image",
+        alt: "web3-converter og-image",
       },
     ],
     site_name: "web3-converter",
-  },
-  twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
   },
 };
 
