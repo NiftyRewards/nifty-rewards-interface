d9797f3018ea339b68d6ed423683fe7eb94d7ba9
aeither <36173828+aeither@users.noreply.github.com>
2022-04-23 01:08:08 +0200

    remove all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,14 @@
 import { Divider, Heading, VStack, Text } from "@chakra-ui/react";
 
-import EstimateGas from "../components/EstimateGas";
-import EtherWeiConverter from "../components/EtherWeiConverter";
-import StringHexConverter from "../components/StringHexConverter";
-import WhiteboardGrid from "../components/WhiteboardGrid";
-
 const Home = () => {
   return (
     <VStack>
-      <EstimateGas />
       <Heading>bytes32/hex - string/utf8</Heading>
       <Text>https://www.epochconverter.com/</Text>
-      <StringHexConverter />
       <Divider py="4" />
       <Heading>wei - ether</Heading>
-      <EtherWeiConverter />
       <Divider py="4" />
       <Heading>Whiteboard</Heading>
-      <WhiteboardGrid />
     </VStack>
   );
 };
