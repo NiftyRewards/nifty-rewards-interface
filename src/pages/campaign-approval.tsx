b2c6ca5baeaaf3f2d32cd5911894bbd9afe57a33
aeither <36173828+aeither@users.noreply.github.com>
2022-04-24 01:11:50 +0200

    web3auth log pkey

--- src/pages/campaign-approval.tsx
+++ src/pages/campaign-approval.tsx
@@ -1,9 +1,12 @@
-import { VStack } from "@chakra-ui/react";
+import { Heading, VStack, HStack } from "@chakra-ui/react";
 
 const CampaignApproval = () => {
-
   return (
     <VStack>
+      <Heading>Create new campaign</Heading>
+      <HStack layerStyle="solid-card">
+          
+      </HStack>
     </VStack>
   );
 };
--- src/views/LoggedIn/LoggedInView.tsx
+++ src/views/LoggedIn/LoggedInView.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { useWeb3Auth } from "../../services/web3auth";
 import { useState, useEffect } from "react";
 import WalletConnectView from "views/LoggedIn/WalletConnectView";
+import Web3 from "web3";
 
 const LoggedInView = () => {
-  const { provider, logout, getUserInfo, getAccounts, getBalance } =
+  const { provider, logout, getUserInfo, getAccounts, getBalance, web3Auth } =
     useWeb3Auth();
   // login,
   // logout,
@@ -20,8 +21,15 @@ const LoggedInView = () => {
 
   const getInfos = async () => {
     const userInfo = await getUserInfo();
-    const accounts = await getAccounts();
+    // const accounts = await getAccounts();
     const balance = await getBalance();
+    const user = await web3Auth.getUserInfo();
+
+    const web3 = new Web3(web3Auth.provider);
+    const accounts = await web3.eth.getAccounts();
+    console.log("pubKey", accounts); // <-- the public key
+
+    console.log({ user });
     console.log({ userInfo });
     console.log({ accounts });
     console.log({ balance });
