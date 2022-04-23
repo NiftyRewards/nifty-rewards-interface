bfa382ba185b01aa898c017d315281306766d8d8
aeither <36173828+aeither@users.noreply.github.com>
2022-04-23 02:01:25 +0200

    format all

---
+++ src/components/WalletConnect.tsx
@@ -0,0 +1,57 @@
+import WalletConnectClient, { CLIENT_EVENTS } from "@walletconnect/client";
+import EthereumProvider from "@walletconnect/ethereum-provider";
+import { PairingTypes, SessionTypes } from "@walletconnect/types";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+
+const WalletConnect = () => { 
+    
+    
+    const first = async () => { 
+
+// 1. Create a WalletConnect Client
+const client = await WalletConnectClient.init({
+  projectId: "c4f79cc...",
+  relayUrl: "wss://relay.walletconnect.com",
+  metadata: {
+    name: "Example Dapp",
+    description: "Example Dapp",
+    url: "#",
+    icons: ["https://walletconnect.com/walletconnect-logo.png"],
+  },
+});
+
+// 2. Subscribe to client events
+
+client.on(
+  CLIENT_EVENTS.pairing.proposal,
+  async (proposal: PairingTypes.Proposal) => {
+    // Display the QRCode modal on a new pairing request.
+    const { uri } = proposal.signal.params;
+    console.log("EVENT", "QR Code Modal opened");
+    QRCodeModal.open(uri, () => {
+      console.log("EVENT", "QR Code Modal closed");
+    });
+  }
+);
+
+client.on(
+  CLIENT_EVENTS.session.deleted,
+  (deletedSession: SessionTypes.Settled) => {
+    // Perform some cleanup after session was deleted (e.g. via `provider.disconnect()`)
+  }
+);
+
+// 3. Create EthereumProvider (with default RPC configuration) by passing in the `client` instance.
+const provider = new EthereumProvider({
+  chainId: 1,
+  client,
+  rpc: {
+    infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+  },
+});
+
+// 4. Enable session (triggers `CLIENT_EVENTS.pairing.proposal` event).
+await provider.enable();
+ } }
+
+ export default WalletConnect;
